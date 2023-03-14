4dbd1cb72c7987dfad04aca61e04ac14bfd8bc61
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-03-14 11:46:58 +0300

    Add files via upload

---
+++ simple-functional-programming.js
@@ -0,0 +1,26 @@
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);;
+
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
